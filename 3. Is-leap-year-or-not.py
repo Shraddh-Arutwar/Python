975d4b8c9d0d5e50599a40384bb179e8e83732a4
Shraddha <85822218+Shraddh-Arutwar@users.noreply.github.com>
2021-08-16 01:38:20 +0530

    code update

--- 3. Is-leap-year-or-not.py
+++ 3. Is-leap-year-or-not.py
@@ -16,8 +16,10 @@ def is_leap(year):
     if (year%4 == 0): 
         if (year%400 == 0): 
             leap = True
-        elif (year%100): 
+        elif (year%100 == 0): 
             leap = False
+        else: 
+            leap = True
     
     return leap
 
